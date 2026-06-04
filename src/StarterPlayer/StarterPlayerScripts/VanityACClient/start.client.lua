b1d247b62eb24fde1071d4de05c2c410b7f4477c
lfisher447-afk <lfisher447@gmail.com>
2026-06-04 18:16:43 -0500

    Add vanity anti-cheat client startup script

---
+++ src/StarterPlayer/StarterPlayerScripts/VanityACClient/start.client.lua
@@ -0,0 +1,8 @@
+-- [[ VANITY-ANTICHEAT CLIENT STARTUP SCRIPT V2.5 ROBLOX ENHANCED ]]
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local vanityAntiCheatClient = require(script.Parent)
+vanityAntiCheatClient.init()
+
+print("[VANITY-ANTICHEAT-CLIENT] Anti-cheat client started!")
