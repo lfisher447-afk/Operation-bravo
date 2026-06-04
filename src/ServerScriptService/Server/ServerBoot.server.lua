d82525d49c74804ebb5e03eca1f2e5489767826b
lfisher447-afk <lfisher447@gmail.com>
2026-06-04 18:57:58 -0500

    Update ServerBoot.server.lua

--- src/ServerScriptService/Server/ServerBoot.server.lua
+++ src/ServerScriptService/Server/ServerBoot.server.lua
@@ -1,10 +1,12 @@
 --!strict
 local ServerScriptService = game:GetService('ServerScriptService')
-local ServerApp = ServerScriptService:WaitForChild('App')
+local ServerApp = script.Parent -- Refers directly to ServerScriptService.Server
 local Runner = require(ServerApp.Modules.ServiceRunner)
+
 local services = {
     require(ServerApp.Services.QBCoreService),
     require(ServerApp.Services.WeaponService),
     require(ServerApp.Services.AimTrainerService)
 }
+
 Runner.boot(services)
